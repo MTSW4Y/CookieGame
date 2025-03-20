027863d97449dabca1fb4472d98bca52288b1b1f
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-20 22:31:42 +0100

    Database aanmaken

--- database.py
+++ database.py
@@ -4,3 +4,16 @@ DB_PATH = 'central_database.db'
 
 def get_connection():
     return sqlite3.connect(DB_PATH)
+
+def add_order(customer, stroopwafels, prince_koeken, orios):
+    with get_connection() as conn:
+        conn.execute(
+            'INSERT INTO orders (customer, stroopwafels, prince_koeken, orios) VALUES (?, ?, ?, ?)',
+            (customer, stroopwafels, prince_koeken, orios)
+        )
+        conn.commit()
+
+# Voeg hier alvast drie orders toe
+add_order('AH', 3, 16, 0)
+add_order('Lidl', 0, 0, 25)
+add_order('Jamin', 5, 5, 5)
