1bfbdc918f0a1a29ddafbe891b4e9aa83a85d183
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-20 23:16:46 +0100

    Melding uit

--- pages/order_management.py
+++ pages/order_management.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import time
 from database import get_orders
-from app import new_order_message, last_order_time
+from app import new_order_message
 
 st.title('Order Management')
 
