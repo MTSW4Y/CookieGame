6a421bead27f9a3ca1f938ff135988ba2994b63b
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-21 00:24:39 +0100

    Omschrijving functionaliteit

--- pages/customers.py
+++ pages/customers.py
@@ -1,4 +1,4 @@
 import streamlit as st
 
 st.title('Customers')
-st.write('Hello World from Customers Page')
+st.write('Hier kunnen de klanten straks hun orders gereedmelden en aangeven of ze compleet waren en of de kwaliteit voldoende was')
