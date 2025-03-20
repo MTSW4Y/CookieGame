9f70bc622dc350b667fecb5798052d2257ebbfc8
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-20 22:32:40 +0100

    database aanmaken

--- pages/order_management.py
+++ pages/order_management.py
@@ -1,3 +1,10 @@
 import streamlit as st
 st.title('Order Management')
-st.write('Hello World from Order Management Page')
+
+# Toon bestaande orders
+with get_connection() as conn:
+    orders = conn.execute('SELECT * FROM orders').fetchall()
+
+st.write("### Existing Orders")
+for order in orders:
+    st.write(order)
