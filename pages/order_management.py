c5926f61c5414add6404b937480921b0e43e3aa4
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-21 00:09:53 +0100

    Bericht eruit gehaald

--- pages/order_management.py
+++ pages/order_management.py
@@ -5,16 +5,10 @@ from database import get_orders
 
 st.title('Order Management')
 
-# Toon nieuwe order melding
-# if new_order_message and last_order_time:
-#     if time.time() - last_order_time <= 10:  # Meldingen worden 10 seconden weergegeven
-#         st.success(new_order_message)
-
-# Toon bestaande orders met automatische verversing
 st.write("### Openstaande Orders")
 orders_placeholder = st.empty()
 
 while True:
     orders_df = get_orders()
     orders_placeholder.dataframe(orders_df)
-    time.sleep(5)  # Ververs de tabel elke 5 seconden
+    time.sleep(10)  # Ververs de tabel elke 5 seconden
