bcae0620daf863fcff4ab88694f2f3afa0bbf7c9
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-21 13:15:04 +0100

    autorefresh

--- pages/order_management.py
+++ pages/order_management.py
@@ -1,10 +1,10 @@
 import streamlit as st
+from streamlit_autorefresh import st_autorefresh
 from database import get_orders
-# from app import new_order_message
 
 st.title('Order Management')
 
 st.write("### Openstaande Orders")
 orders_df = get_orders()
 st.dataframe(orders_df)
-st.autorefresh(interval=5000, key="order_refresh")
+st_autorefresh(interval=5000, key="order_refresh")
