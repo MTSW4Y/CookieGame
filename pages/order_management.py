7111d43b8e95f2ed75df3de65216939d01c47a60
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-03-26 17:51:21 +0100

    Refresh op andere plek

--- pages/order_management.py
+++ pages/order_management.py
@@ -4,10 +4,10 @@ from database import get_orders
 
 st.title('Order Management')
 
+st_autorefresh(interval=5000, key="order_refresh")
 st.write("### Dag en tijd")
 st.write(f"Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
 
 st.write("### Openstaande Orders")
 orders_df = get_orders()
 st.dataframe(orders_df)
-st_autorefresh(interval=5000, key="order_refresh")
