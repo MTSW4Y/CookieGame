882fa8bfbf52bc40336d48693a9e232193d2f5ab
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-08 17:27:05 +0200

    Update customers.py

--- pages/customers.py
+++ pages/customers.py
@@ -1,4 +1,6 @@
 import streamlit as st
-
+from database import add_order
 st.title('Customers')
 st.write('Hier kunnen de klanten straks hun orders gereedmelden en aangeven of ze compleet waren en of de kwaliteit voldoende was')
+
+add_order("Patty", 1, 2, 3)
