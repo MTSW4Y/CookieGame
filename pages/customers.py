7f3b2617302f6f72de77c80fdbaeee5a5bd820c4
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-08 17:39:19 +0200

    Update customers.py

--- pages/customers.py
+++ pages/customers.py
@@ -6,10 +6,27 @@ st.write('Hier kunnen de klanten straks hun orders gereedmelden en aangeven of z
 
 
 # add_order(customer, stroopwafels, prince_koeken, orios)
+
 def start_game():
   add_order("Hema", 2, 2, 15)
   add_order("Jumbo", 2, 0, 3)
   add_order("AH", 3, 0, 2)
   add_order("Hema", 3, 3, 0)
 
+def dag1():
+  add_order("Hema", 2, 2, 15)
+
+def dag2():
+  add_order("Hema", 2, 2, 15)
+
+def dag3():
+  add_order("Hema", 2, 2, 15)
+
+def dag4():
+  add_order("Hema", 2, 2, 15)
+
 st.button('Start', on_click=start_game)
+st.button('na dag 1', on_click=dag1)
+st.button('na dag 2', on_click=dag1)
+st.button('na dag 3', on_click=dag1)
+st.button('na dag 4', on_click=dag1)
