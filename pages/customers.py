77d60c73928c9c1dcc8f003235cc63d8e4c97967
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-08 17:35:35 +0200

    Update customers.py

--- pages/customers.py
+++ pages/customers.py
@@ -3,8 +3,13 @@ from database import add_order
 st.title('Customers')
 st.write('Hier kunnen de klanten straks hun orders gereedmelden en aangeven of ze compleet waren en of de kwaliteit voldoende was')
 
+
+
 # add_order(customer, stroopwafels, prince_koeken, orios)
-add_order("Hema", 2, 2, 15)
-add_order("Jumbo", 2, 0, 3)
-add_order("AH", 3, 0, 2)
-add_order("Hema", 3, 3, 0)
+def start_game:
+  add_order("Hema", 2, 2, 15)
+  add_order("Jumbo", 2, 0, 3)
+  add_order("AH", 3, 0, 2)
+  add_order("Hema", 3, 3, 0)
+
+st.button('Start', on_click=start_game)
