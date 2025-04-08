360ea468b1baa6a915d26eb829e1e269d0cf04f3
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-08 14:24:10 +0200

    timer

--- pages/timer.py
+++ pages/timer.py
@@ -1,1 +1,33 @@
+import streamlit as st
 
+# Timer settings
+if 'timer_running' not in st.session_state:
+    st.session_state.timer_running = False
+    st.session_state.day_count = 1
+    st.session_state.current_time = datetime.strptime('09:00', '%H:%M')
+    st.session_state.start_time = None
+
+def start_timer():
+    st.session_state.timer_running = True
+    if st.session_state.start_time:
+        pass
+    else:
+        st.session_state.start_time = time.time()
+
+def stop_timer():
+    st.session_state.timer_running = False
+
+def reset_timer():
+    st.session_state.day_count = 1
+    st.session_state.start_time = None
+
+# Update the timer
+if st.session_state.timer_running:
+    elapsed_real_time = time.time() - st.session_state.start_time
+    elapsed_game_time = timedelta(seconds=elapsed_real_time * 120)  # 4 minuten = 8 uur
+    st.session_state.current_time = datetime.strptime('09:00', '%H:%M') + elapsed_game_time
+
+    if st.session_state.current_time.strftime('%H:%M') >= '17:00':
+        st.session_state.day_count += 1
+        st.session_state.current_time = datetime.strptime('09:00', '%H:%M')
+        st.session_state.start_time = time.time()  # Reset de starttijd
