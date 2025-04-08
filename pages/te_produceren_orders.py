b1a7a1d4c647e38c1915065870a24fc945974019
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-08 21:02:02 +0200

    Update te_produceren_orders.py

--- pages/te_produceren_orders.py
+++ pages/te_produceren_orders.py
@@ -7,7 +7,7 @@ st.title('Order Management')
 st_autorefresh(interval=5000, key="order_refresh")
 
 st.write("### Dag en tijd")
-st.write(f"##Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
+st.write(f"## Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
 
 st.write("### Openstaande Orders")
 orders_df = get_orders()
