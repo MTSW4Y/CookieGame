36de71257831e68e7b621a7de9f0213560709c02
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-09 10:44:35 +0200

    Update timer.py

--- pages/timer.py
+++ pages/timer.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 from datetime import datetime, timedelta
 from streamlit_autorefresh import st_autorefresh
-from database import get_orders, clear_orders
+from database import get_orders, clear_orders, add_time, get_times
 import threading
 
 # Timer settings
@@ -71,7 +71,8 @@ with col2:
     st.button('Reset Timer', on_click=reset_timer)
 
 st.write(f"Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
+add_time(f"Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
 
-st.write("### Openstaande Orders")
-orders_df = get_orders()
-st.dataframe(orders_df)
+st.write("### Tijden")
+# orders_df = get_orders()
+st.dataframe(get_times())
