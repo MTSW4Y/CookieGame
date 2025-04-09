0a79c0d165f9f6df2617236394e2a9929e836ee6
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-04-09 10:42:07 +0200

    Update database.py

--- database.py
+++ database.py
@@ -1,5 +1,7 @@
 import sqlite3
 import pandas as pd
+import time
+# from datetime import datetime
 
 DB_PATH = 'central_database.db'
 
@@ -22,3 +24,19 @@ def clear_orders():
     with get_connection() as conn:
         conn.execute('DELETE FROM orders')
         conn.commit()
+
+def add_time(simulation_time):
+    # clock_timestamp = int(time.time())  # Unix timestamp
+    clock_timestamp = time.time()
+    with get_connection() as conn:
+        conn.execute(
+            'INSERT INTO time (simulation_time, clock_time) VALUES (?, ?)',
+            (simulation_time, clock_timestamp)
+        )
+        conn.commit()
+
+def get_times():
+    with get_connection() as conn:
+        df = pd.read_sql('SELECT * FROM time', conn)
+        # df['clock_time'] = pd.to_datetime(df['clock_time'], unit='s')  # omzetting naar leesbare datum
+        return df
