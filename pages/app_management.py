d3f5976187d13a1a11ab939ef9be679b8c77eeee
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-15 17:09:56 +0200

    Update app_management.py

--- pages/app_management.py
+++ pages/app_management.py
@@ -84,7 +84,8 @@ st.button('na dag 4', on_click=dag4)
 
 # Voeg een tekstveld toe
 naam = st.text_input("Vul je naam in:")
+beroep = st.text_input("Vul je beroep in:")
 
 # Gebruik de waarde ergens
 if naam:
-    st.write(f"Hallo, {naam}!")
+    st.write(f"Hallo, {naam} je beroep is: {beroep}")
