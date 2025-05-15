66b60350a682da00c176ac17520bfb007bc21745
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-15 16:06:01 +0200

    Update orders.py

--- orders.py
+++ orders.py
@@ -1,1 +1,19 @@
-orders
+from database import add_order
+
+def start_game():
+  add_order("Jumbo", 0, 8, 4)
+  add_order("AH", 0, 4, 2)
+  add_order("Jumbo", 0, 5, 3)
+  add_order("Hema", 2, 0, 4)
+
+def dag1():
+  add_order("Hema", 2, 3, 2)
+  
+def dag2():
+  add_order("Jumbo", 0, 4, 3)
+  
+def dag3():
+  add_order("AH", 0, 5, 2)
+  
+def dag4():
+  add_order("Hema", 3, 0, 6)
