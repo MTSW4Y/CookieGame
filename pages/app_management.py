22d8bbf2a03cc9d2e29494dfe4d60a81828ef39e
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-15 20:41:36 +0200

    Update app_management.py

--- pages/app_management.py
+++ pages/app_management.py
@@ -1,4 +1,4 @@
-pimport streamlit as st
+import streamlit as st
 import time
 from datetime import datetime, timedelta
 from streamlit_autorefresh import st_autorefresh
@@ -82,13 +82,13 @@ st.button('na dag 2', on_click=dag2)
 st.button('na dag 3', on_click=dag3)
 st.button('na dag 4', on_click=dag4)
 
-# # Voeg een tekstveld toe
-# naam = st.text_input("Vul je naam in:")
-# beroep = st.text_input("Vul je beroep in:")
-# selectie = st.selectbox("Vul de klant in", ["Jumbo", "AH", "Hema"])
-# # stroopwafels = st.number_input("Vul het aantal stroopwafels in voor deze bestelling", min_value=0, max_value=10, step=1)
-# # oreos = st.number_input("Vul het aantal oreos in voor deze bestelling", min_value=0, max_value=10, step=1)
-# # prince_koeken = st.number_input("Vul het aantal prince koeken in voor deze bestelling", min_value=0, max_value=10, step=1)
+# Voeg een tekstveld toe
+naam = st.text_input("Vul je naam in:")
+beroep = st.text_input("Vul je beroep in:")
+selectie = st.selectbox("Vul de klant in", ["Jumbo", "AH", "Hema"])
+stroopwafels = st.number_input("Vul het aantal stroopwafels in voor deze bestelling", min_value=0, max_value=10, step=1)
+oreos = st.number_input("Vul het aantal oreos in voor deze bestelling", min_value=0, max_value=10, step=1)
+prince_koeken = st.number_input("Vul het aantal prince koeken in voor deze bestelling", min_value=0, max_value=10, step=1)
 # stroopwafels = st.number_input("Vul het aantal stroopwafels in voor deze bestelling")
 # oreos = st.number_input("Vul het aantal oreos in voor deze bestelling")
 # prince_koeken = st.number_input("Vul het aantal prince koeken in voor deze bestelling")
