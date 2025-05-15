88341f4f42a714706450c34147e25e2ac4148901
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-15 16:50:33 +0200

    Update order_management.py

--- pages/order_management.py
+++ pages/order_management.py
@@ -1,23 +1,23 @@
-import streamlit as st
-from streamlit_autorefresh import st_autorefresh
-from database import get_orders
+# import streamlit as st
+# from streamlit_autorefresh import st_autorefresh
+# from database import get_orders
 
-def start_timer():
-  pass
+# def start_timer():
+#   pass
 
-def reset_timer():
-  pass
+# def reset_timer():
+#   pass
 
-st.title('Order Management')
+# st.title('Order Management')
 
-st_autorefresh(interval=5000, key="order_refresh")
+# st_autorefresh(interval=5000, key="order_refresh")
 
-st.button('Start Timer', on_click=start_timer)
-st.button('Reset', on_click=reset_timer)
+# st.button('Start Timer', on_click=start_timer)
+# st.button('Reset', on_click=reset_timer)
 
-st.write("### Dag en tijd")
-st.write(f"Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
+# st.write("### Dag en tijd")
+# st.write(f"Dag {st.session_state.day_count} - Tijd: {st.session_state.current_time.strftime('%H:%M')}")
 
-st.write("### Openstaande Orders")
-orders_df = get_orders()
-st.dataframe(orders_df.reset_index(drop=True))
+# st.write("### Openstaande Orders")
+# orders_df = get_orders()
+# st.dataframe(orders_df.reset_index(drop=True))
