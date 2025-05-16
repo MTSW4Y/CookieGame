934c9728de31f6d963f895ed772c8a43c8ce829c
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-16 14:48:34 +0200

    Update app_management.py

--- pages/app_management.py
+++ pages/app_management.py
@@ -4,6 +4,30 @@ from datetime import datetime, timedelta
 from streamlit_autorefresh import st_autorefresh
 from database import get_orders, clear_orders, upsert_time, get_simulation_time, add_order
 
+#########################ORDERS##################################
+
+orders = [
+    {"Uur": 8, "Klant": "Hema", "Due": 2, "Stroopwafels": 6, "Prince": 3, "Oreos": 13},
+    {"Uur": 8, "Klant": "Jumbo", "Due": 2, "Stroopwafels": 0, "Prince": 8, "Oreos": 19},
+    {"Uur": 8, "Klant": "AH", "Due": 3, "Stroopwafels": 0, "Prince": 5, "Oreos": 30},
+    {"Uur": 8, "Klant": "Hema", "Due": 3, "Stroopwafels": 9, "Prince": 0, "Oreos": 19},
+    {"Uur": 9, "Klant": "Jumbo", "Due": 4, "Stroopwafels": 0, "Prince": 3, "Oreos": 13},
+    {"Uur": 9, "Klant": "AH", "Due": 4, "Stroopwafels": 0, "Prince": 4, "Oreos": 13},
+    {"Uur": 10, "Klant": "Jumbo", "Due": 5, "Stroopwafels": 0, "Prince": 5, "Oreos": 19},
+    {"Uur": 10, "Klant": "Hema", "Due": 5, "Stroopwafels": 6, "Prince": 0, "Oreos": 25},
+    {"Uur": 11, "Klant": "Jumbo", "Due": 6, "Stroopwafels": 0, "Prince": 3, "Oreos": 19},
+    {"Uur": 11, "Klant": "AH", "Due": 6, "Stroopwafels": 0, "Prince": 4, "Oreos": 19},
+    {"Uur": 13, "Klant": "Hema", "Due": 7, "Stroopwafels": 4, "Prince": 0, "Oreos": 25},
+    {"Uur": 13, "Klant": "AH", "Due": 7, "Stroopwafels": 0, "Prince": 5, "Oreos": 19},
+    {"Uur": 14, "Klant": "Jumbo", "Due": 8, "Stroopwafels": 0, "Prince": 8, "Oreos": 16},
+    {"Uur": 14, "Klant": "Hema", "Due": 8, "Stroopwafels": 4, "Prince": 0, "Oreos": 20}
+]
+    
+def schiet_nieuwe_orders_in():
+    for order in orders:
+        if order['Uur'] == st.session_state.last_hour:
+            add_order(order['Klant'], f"Levermoment {order['Due']}", order['Stroopwafels'], order['Prince'], order['Oreos'])
+
 #########################TIMER############################
 
 def start_timer():
@@ -45,51 +69,6 @@ else:
     upsert_time(f"Levermoment {st.session_state.delivery_slot}: {st.session_state.current_time.strftime('%H:%M')}")
 due_date = f"Levermoment {st.session_state.delivery_slot+2}"
 spoed_due_date = f"Levermoment {st.session_state.delivery_slot+1}"
-
-#########################ORDERS##################################
-
-orders = [
-    {"Uur": 8, "Klant": "Hema", "Due": 2, "Stroopwafels": 6, "Prince": 3, "Oreos": 13},
-    {"Uur": 8, "Klant": "Jumbo", "Due": 2, "Stroopwafels": 0, "Prince": 8, "Oreos": 19},
-    {"Uur": 8, "Klant": "AH", "Due": 3, "Stroopwafels": 0, "Prince": 5, "Oreos": 30},
-    {"Uur": 8, "Klant": "Hema", "Due": 3, "Stroopwafels": 9, "Prince": 0, "Oreos": 19},
-    {"Uur": 9, "Klant": "Jumbo", "Due": 4, "Stroopwafels": 0, "Prince": 3, "Oreos": 13},
-    {"Uur": 9, "Klant": "AH", "Due": 4, "Stroopwafels": 0, "Prince": 4, "Oreos": 13},
-    {"Uur": 10, "Klant": "Jumbo", "Due": 5, "Stroopwafels": 0, "Prince": 5, "Oreos": 19},
-    {"Uur": 10, "Klant": "Hema", "Due": 5, "Stroopwafels": 6, "Prince": 0, "Oreos": 25},
-    {"Uur": 11, "Klant": "Jumbo", "Due": 6, "Stroopwafels": 0, "Prince": 3, "Oreos": 19},
-    {"Uur": 11, "Klant": "AH", "Due": 6, "Stroopwafels": 0, "Prince": 4, "Oreos": 19},
-    {"Uur": 13, "Klant": "Hema", "Due": 7, "Stroopwafels": 4, "Prince": 0, "Oreos": 25},
-    {"Uur": 13, "Klant": "AH", "Due": 7, "Stroopwafels": 0, "Prince": 5, "Oreos": 19},
-    {"Uur": 14, "Klant": "Jumbo", "Due": 8, "Stroopwafels": 0, "Prince": 8, "Oreos": 16},
-    {"Uur": 14, "Klant": "Hema", "Due": 8, "Stroopwafels": 4, "Prince": 0, "Oreos": 20}
-]
-    
-def schiet_nieuwe_orders_in():
-    for order in orders:
-        if order['Uur'] == st.session_state.last_hour:
-            add_order(order['Klant'], f"Levermoment {order['Due']}", order['Stroopwafels'], order['Prince'], order['Oreos'])
-
-# def start_game():
-#   add_order("Jumbo", due_date, 0, 8, 4)
-#   add_order("AH", due_date, 0, 4, 2)
-#   add_order("Jumbo", due_date, 0, 5, 3)
-#   add_order("Hema", due_date, 2, 0, 4)
-
-# def dag1():
-#   add_order("Hema", due_date, 2, 3, 2)
-  
-# def dag2():
-#   add_order("Jumbo", due_date, 0, 4, 3)
-  
-# def dag3():
-#   add_order("AH", due_date, 0, 5, 2)
-  
-# def dag4():
-#   add_order("Hema", due_date, 3, 0, 6)
-
-# def bestel(klant, stroopwafel, oreo, prince):
-#   add_order(klant, spoed_due_date, stroopwafel, prince, oreo)
 
 #########################LAY-OUT##################################
 
