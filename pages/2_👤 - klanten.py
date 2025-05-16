c7349ee1eb16a194fe7bd4895ce0aab3fc78d5ce
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-16 09:17:43 +0200

    Update 2_👤 - klanten.py

--- pages/2_👤 - klanten.py
+++ pages/2_👤 - klanten.py
@@ -13,14 +13,14 @@ with col1:
     
 with col2:
     st.write("### Geleverd")
-    gel_aant_stroopwafels = st.number_input("Aantal stroopwafels", min_value=0, max_value=10, step=1)
-    gel_aant_oreos = st.number_input("Aantal oreos", min_value=0, max_value=10, step=1)
-    gel_aant_prince_koeken = st.number_input("Aantal prince koeken", min_value=0, max_value=10, step=1)
+    gel_aant_stroopwafels = st.number_input("Aantal stroopwafels", step=1)
+    gel_aant_oreos = st.number_input("Aantal oreos", step=1)
+    gel_aant_prince_koeken = st.number_input("Aantal prince koeken", step=1)
 
 with col3:
     st.write("### Kwaliteit")
-    kwal_stroopwafels = st.number_input("Goede stroopwafels", min_value=0, max_value=10, step=1)
-    kwal_oreos = st.number_input("Goede oreos", min_value=0, max_value=10, step=1)
-    kwal_prince_koeken = st.number_input("Goede prince koeken", min_value=0, max_value=10, step=1)
+    kwal_stroopwafels = st.number_input("Goede stroopwafels", step=1, value=gel_aant_stroopwafels)
+    kwal_oreos = st.number_input("Goede oreos", step=1, value=gel_aant_oreos)
+    kwal_prince_koeken = st.number_input("Goede prince koeken", step=1, value=gel_aant_prince_koeken)
 
 st.button('Registeer', on_click=lambda: bestel(klant, stroopwafels, oreos, prince_koeken))
