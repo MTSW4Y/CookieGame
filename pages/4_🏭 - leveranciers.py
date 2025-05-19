75ba95cda6d9815565eeb696eee0272e7e0fdd7f
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-19 16:16:19 +0200

    Update 4_🏭 - leveranciers.py

--- pages/4_🏭 - leveranciers.py
+++ pages/4_🏭 - leveranciers.py
@@ -1,6 +1,14 @@
 import streamlit as st
 from database import add_supply, get_supplies
 
+def registreer(groep, gel_aant_stroopwafels_vul, gel_aant_prince_koeken_vul, gel_aant_oreos_vul, gel_aant_stroopwafels_buit, gel_aant_prince_koeken_buit, gel_aant_oreos_buit):
+    add_supply(groep, gel_aant_stroopwafels_vul)
+    add_supply(groep, gel_aant_prince_koeken_vul)
+    add_supply(groep, gel_aant_oreos_vul)
+    add_supply(groep, gel_aant_stroopwafels_buit)
+    add_supply(groep, gel_aant_prince_koeken_buit)
+    add_supply(groep, gel_aant_oreos_buit)
+
 st.title('🏭 - Leveranciers')
 
 st.write("### Uitgifte materialen")
@@ -8,23 +16,22 @@ st.write("### Uitgifte materialen")
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.write("### Orderinfo")
+    st.write("### Groep")
     groep = st.number_input("Groepsnummer", step=1)
-    ordernummer = st.number_input("Ordernummer", step=1)
     
 with col2:
-    st.write("### Geleverd")
-    gel_aant_stroopwafels = st.number_input("Aantal stroopwafels", step=1)
-    gel_aant_prince_koeken = st.number_input("Aantal prince koeken", step=1)
-    gel_aant_oreos = st.number_input("Aantal oreos", step=1)
+    st.write("### Vulling")
+    gel_aant_stroopwafels_vul = st.number_input("Aantal stroopwafelvulling", step=1)
+    gel_aant_prince_koeken_vul = st.number_input("Aantal princevulling", step=1)
+    gel_aant_oreos_vul = st.number_input("Aantal oreovulling", step=1, value=gel_aant_oreos)
 
 with col3:
-    st.write("### Kwaliteit")
-    kwal_stroopwafels = st.number_input("Goede stroopwafels", step=1, value=gel_aant_stroopwafels)
-    kwal_prince_koeken = st.number_input("Goede prince koeken", step=1, value=gel_aant_prince_koeken)
-    kwal_oreos = st.number_input("Goede oreos", step=1, value=gel_aant_oreos)
+    st.write("### Buitenkant")
+    gel_aant_stroopwafels_buit = st.number_input("Aantal stroopwafel-buitenkanten", step=1, value=gel_aant_stroopwafels_vul*2)
+    gel_aant_prince_koeken_buit = st.number_input("Aantal prince koek-buitenkanten", step=1, value=gel_aant_prince_koeken_vul*2)
+    gel_aant_oreos_buit = st.number_input("Aantal oreo-buitenkanten", step=1, value=gel_aant_oreos_vul*2)
 
-if st.button('Registeer', on_click=lambda: add_ready_order(ordernummer, groep, gel_aant_stroopwafels, gel_aant_prince_koeken, gel_aant_oreos, kwal_stroopwafels, kwal_prince_koeken, kwal_oreos)):
-    st.toast("Kwaliteit geregistreert", icon="✅")
+if st.button('Registeer', on_click=lambda: registreer(groep, gel_aant_stroopwafels_vul, gel_aant_prince_koeken_vul, gel_aant_oreos_vul, gel_aant_stroopwafels_buit, gel_aant_prince_koeken_buit, gel_aant_oreos_buit)):
+    st.toast("Inkoop geregistreert", icon="✅")
 
-st.dataframe(get_ready_orders())
+st.dataframe(get_supplies())
