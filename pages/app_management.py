0f6db335fd6970cabda6b8ce80c7dbf350c70949
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-21 04:27:54 +0200

    Update app_management.py

--- pages/app_management.py
+++ pages/app_management.py
@@ -7,20 +7,20 @@ from database import get_orders, clear_orders, upsert_time, get_simulation_time,
 #########################ORDERS##################################
 
 orders = [
-    {"Uur": 8, "Klant": "Hema", "Due": 2, "Stroopwafels": 6, "Prince": 3, "Oreos": 13},
-    {"Uur": 8, "Klant": "Jumbo", "Due": 2, "Stroopwafels": 0, "Prince": 8, "Oreos": 19},
-    {"Uur": 8, "Klant": "AH", "Due": 3, "Stroopwafels": 0, "Prince": 5, "Oreos": 30},
-    {"Uur": 8, "Klant": "Hema", "Due": 3, "Stroopwafels": 9, "Prince": 0, "Oreos": 19},
-    {"Uur": 9, "Klant": "Jumbo", "Due": 4, "Stroopwafels": 0, "Prince": 3, "Oreos": 13},
-    {"Uur": 9, "Klant": "AH", "Due": 4, "Stroopwafels": 0, "Prince": 4, "Oreos": 13},
-    {"Uur": 10, "Klant": "Jumbo", "Due": 5, "Stroopwafels": 0, "Prince": 5, "Oreos": 19},
-    {"Uur": 10, "Klant": "Hema", "Due": 5, "Stroopwafels": 6, "Prince": 0, "Oreos": 25},
-    {"Uur": 11, "Klant": "Jumbo", "Due": 6, "Stroopwafels": 0, "Prince": 3, "Oreos": 19},
-    {"Uur": 11, "Klant": "AH", "Due": 6, "Stroopwafels": 0, "Prince": 4, "Oreos": 19},
-    {"Uur": 13, "Klant": "Hema", "Due": 7, "Stroopwafels": 4, "Prince": 0, "Oreos": 25},
-    {"Uur": 13, "Klant": "AH", "Due": 7, "Stroopwafels": 0, "Prince": 5, "Oreos": 19},
-    {"Uur": 14, "Klant": "Jumbo", "Due": 8, "Stroopwafels": 0, "Prince": 8, "Oreos": 16},
-    {"Uur": 14, "Klant": "Hema", "Due": 8, "Stroopwafels": 4, "Prince": 0, "Oreos": 20}
+    {"Uur": 8, "Klant": "Hema", "Due": 2, "Stroopwafels": 6, "Prince": 3, "Penny_wafels": 13},
+    {"Uur": 8, "Klant": "Jumbo", "Due": 2, "Stroopwafels": 0, "Prince": 8, "Penny_wafels": 19},
+    {"Uur": 8, "Klant": "AH", "Due": 3, "Stroopwafels": 0, "Prince": 5, "Penny_wafels": 30},
+    {"Uur": 8, "Klant": "Hema", "Due": 3, "Stroopwafels": 9, "Prince": 0, "Penny_wafels": 19},
+    {"Uur": 9, "Klant": "Jumbo", "Due": 4, "Stroopwafels": 0, "Prince": 3, "Penny_wafels": 13},
+    {"Uur": 9, "Klant": "AH", "Due": 4, "Stroopwafels": 0, "Prince": 4, "Penny_wafels": 13},
+    {"Uur": 10, "Klant": "Jumbo", "Due": 5, "Stroopwafels": 0, "Prince": 5, "Penny_wafels": 19},
+    {"Uur": 10, "Klant": "Hema", "Due": 5, "Stroopwafels": 6, "Prince": 0, "Penny_wafels": 25},
+    {"Uur": 11, "Klant": "Jumbo", "Due": 6, "Stroopwafels": 0, "Prince": 3, "Penny_wafels": 19},
+    {"Uur": 11, "Klant": "AH", "Due": 6, "Stroopwafels": 0, "Prince": 4, "Penny_wafels": 19},
+    {"Uur": 13, "Klant": "Hema", "Due": 7, "Stroopwafels": 4, "Prince": 0, "Penny_wafels": 25},
+    {"Uur": 13, "Klant": "AH", "Due": 7, "Stroopwafels": 0, "Prince": 5, "Penny_wafels": 19},
+    {"Uur": 14, "Klant": "Jumbo", "Due": 8, "Stroopwafels": 0, "Prince": 8, "Penny_wafels": 16},
+    {"Uur": 14, "Klant": "Hema", "Due": 8, "Stroopwafels": 4, "Prince": 0, "Penny_wafels": 20}
 ]
 
 # dit moet ik nog even opslimmen..........
@@ -28,12 +28,12 @@ orders = [
 def schiet_eerste_order_in():
     for order in orders:
         if order['Uur'] == st.session_state.last_hour:
-            add_order(order['Klant'], f"Levermoment {order['Due']}", order['Stroopwafels'], order['Prince'], order['Oreos'])
+            add_order(order['Klant'], f"Levermoment {order['Due']}", order['Stroopwafels'], order['Prince'], order['Penny_wafels'])
             
 def schiet_nieuwe_orders_in():
     for order in orders:
         if order['Uur'] == st.session_state.last_hour + 1:
-            add_order(order['Klant'], f"Levermoment {order['Due']}", order['Stroopwafels'], order['Prince'], order['Oreos'])
+            add_order(order['Klant'], f"Levermoment {order['Due']}", order['Stroopwafels'], order['Prince'], order['Penny_wafels'])
 
 #########################TIMER############################
 
@@ -102,9 +102,9 @@ col5, col6 = st.columns(2)
     
 with col5:
     stroopwafels = st.number_input("Vul het aantal stroopwafels in voor deze bestelling", min_value=0, max_value=10, step=1)
-    oreos = st.number_input("Vul het aantal oreos in voor deze bestelling", min_value=0, max_value=10, step=1)
+    Penny_wafels = st.number_input("Vul het aantal Penny_wafels in voor deze bestelling", min_value=0, max_value=10, step=1)
     prince_koeken = st.number_input("Vul het aantal prince koeken in voor deze bestelling", min_value=0, max_value=10, step=1)
     
 with col6:
     klant = st.selectbox("Vul de klant in", ["Jumbo", "AH", "Hema"])
-    st.button('Bestel', on_click=lambda: add_order(klant, f"Levermoment {st.session_state.delivery_slot + 1}", stroopwafels, prince_koeken, oreos))
+    st.button('Bestel', on_click=lambda: add_order(klant, f"Levermoment {st.session_state.delivery_slot + 1}", stroopwafels, prince_koeken, Penny_wafels))
