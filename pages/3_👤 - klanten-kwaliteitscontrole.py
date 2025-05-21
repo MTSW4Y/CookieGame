712f051344841c9237e6b4f1b94348face8b9b92
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-21 04:36:03 +0200

    Update 3_👤 - klanten-kwaliteitscontrole.py

--- pages/3_👤 - klanten-kwaliteitscontrole.py
+++ pages/3_👤 - klanten-kwaliteitscontrole.py
@@ -16,13 +16,13 @@ with col2:
     st.write("### Geleverd")
     gel_aant_stroopwafels = st.number_input("Aantal stroopwafels", step=1)
     gel_aant_prince_koeken = st.number_input("Aantal prince koeken", step=1)
-    gel_aant_penny_wafels = st.number_input("Aantal penny_wafels", step=1)
+    gel_aant_penny_wafels = st.number_input("Aantal penny wafels", step=1)
 
 with col3:
     st.write("### Kwaliteit")
     kwal_stroopwafels = st.number_input("Goede stroopwafels", step=1, value=gel_aant_stroopwafels)
     kwal_prince_koeken = st.number_input("Goede prince koeken", step=1, value=gel_aant_prince_koeken)
-    kwal_penny_wafels = st.number_input("Goede penny_wafels", step=1, value=gel_aant_penny_wafels)
+    kwal_penny_wafels = st.number_input("Goede penny wafels", step=1, value=gel_aant_penny_wafels)
 
 if st.button('Registeer', on_click=lambda: add_ready_order(ordernummer, groep, gel_aant_stroopwafels, gel_aant_prince_koeken, gel_aant_penny_wafels, kwal_stroopwafels, kwal_prince_koeken, kwal_penny_wafels)):
     st.toast("Kwaliteit geregistreert", icon="✅")
