766ed862510e602751d00355aa57eb3750e362dc
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-28 15:09:09 +0200

    Update 3_👤 - klanten-kwaliteitscontrole.py

--- pages/3_👤 - klanten-kwaliteitscontrole.py
+++ pages/3_👤 - klanten-kwaliteitscontrole.py
@@ -34,8 +34,10 @@ reject_reason = ""
 if gel_aant_stroopwafels != kwal_stroopwafels or gel_aant_prince_koeken != kwal_prince_koeken or gel_aant_penny_wafels != kwal_penny_wafels:
     st.write(f"### er zijn kwaliteitsissues bij groep {groep}")
     if groep_feedback[groep]:
-        st.write(f"### en groep {groep} wil graag feedback")
+        st.write(f"### er zijn kwaliteitsissues bij groep {groep} en ze willen graag feedback")
         reject_reason = st.selectbox("Wat is de reden voor de afkeur:", ["Vulling zichtbaar", "Lagen los", "Afwijking in vorm", "Overig"])
+    else:
+        st.write(f"### er zijn kwaliteitsissues bij groep {groep}")
 
 if st.button('Registeer', on_click=lambda: add_ready_order(ordernummer, groep, gel_aant_stroopwafels, gel_aant_prince_koeken, gel_aant_penny_wafels, kwal_stroopwafels, kwal_prince_koeken, kwal_penny_wafels, reject_reason)):
     st.toast("Kwaliteit geregistreerd", icon="✅")
