c3ece474e40e671ffb29b663a5c0792adf3b4d25
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-28 16:04:12 +0200

    Update 4_🏭 - leveranciers.py

--- pages/4_🏭 - leveranciers.py
+++ pages/4_🏭 - leveranciers.py
@@ -1,14 +1,14 @@
 import streamlit as st
-from database import add_supply, get_supplies
+from database import add_supply, get_supplies, registrer_supplies
 
-def registreer(groep, gel_aant_stroopwafels_vul, gel_aant_prince_koeken_vul, gel_aant_pennywafels_vul, gel_aant_stroopwafels_buit, gel_aant_prince_koeken_buit, gel_aant_pennywafels_buit):
-    add_supply(groep, 'stroopwafel', 'vulling', gel_aant_stroopwafels_vul)
-    add_supply(groep, 'princekoeken', 'vulling', gel_aant_prince_koeken_vul)
-    add_supply(groep, 'pennywafels', 'vulling', gel_aant_pennywafels_vul)
-    add_supply(groep, 'stroopwafel', 'koekje', gel_aant_stroopwafels_buit)
-    add_supply(groep, 'princekoeken', 'koekje', gel_aant_prince_koeken_buit)
-    add_supply(groep, 'pennywafels', 'koekje', gel_aant_pennywafels_buit)
-    add_supply(groep, 'bakjes', 'rest', gel_aant_bakjes)
+# def registreer(groep, gel_aant_stroopwafels_vul, gel_aant_prince_koeken_vul, gel_aant_pennywafels_vul, gel_aant_stroopwafels_buit, gel_aant_prince_koeken_buit, gel_aant_pennywafels_buit):
+#     add_supply(groep, 'stroopwafel', 'vulling', gel_aant_stroopwafels_vul)
+#     add_supply(groep, 'princekoeken', 'vulling', gel_aant_prince_koeken_vul)
+#     add_supply(groep, 'pennywafels', 'vulling', gel_aant_pennywafels_vul)
+#     add_supply(groep, 'stroopwafel', 'koekje', gel_aant_stroopwafels_buit)
+#     add_supply(groep, 'princekoeken', 'koekje', gel_aant_prince_koeken_buit)
+#     add_supply(groep, 'pennywafels', 'koekje', gel_aant_pennywafels_buit)
+#     add_supply(groep, 'bakjes', 'rest', gel_aant_bakjes)
 
 st.title('🏭 - Leveranciers')
 
@@ -33,7 +33,7 @@ with col3:
     gel_aant_prince_koeken_buit = st.number_input("Aantal 1002 (princekoek deeg)", step=1, value=gel_aant_prince_koeken_vul*2)
     gel_aant_pennywafels_buit = st.number_input("Aantal 1003 (pennywafeldeeg)", step=1, value=gel_aant_pennywafels_vul*2)
 
-if st.button('Registeer', on_click=lambda: registreer(groep, gel_aant_stroopwafels_vul, gel_aant_prince_koeken_vul, gel_aant_pennywafels_vul, gel_aant_stroopwafels_buit, gel_aant_prince_koeken_buit, gel_aant_pennywafels_buit)):
+if st.button('Registeer', on_click=lambda: registrer_supplies(groep, gel_aant_stroopwafels_vul, gel_aant_prince_koeken_vul, gel_aant_pennywafels_vul, gel_aant_stroopwafels_buit, gel_aant_prince_koeken_buit, gel_aant_pennywafels_buit)):
     st.toast("Inkoop geregistreerd", icon="✅")
 
 supplies = get_supplies()
