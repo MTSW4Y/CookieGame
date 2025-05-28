64df5950cb436cc1d80c6ce85431cc4b10eaa50f
Patrick Vercauteren <MTSW4Y@users.noreply.github.com>
2025-05-28 17:09:15 +0200

    Update app_management.py

--- pages/app_management.py
+++ pages/app_management.py
@@ -3,25 +3,11 @@ import time
 from datetime import datetime, timedelta
 from streamlit_autorefresh import st_autorefresh
 from database import get_orders, clear_orders, upsert_time, get_simulation_time, add_order, clear_ready_orders, clear_supplies, upsert_time, add_supply, registrer_supplies
+from orders import week_1, week_2, week_3
 
 #########################ORDERS##################################
 
-orders = [
-    {"Uur": 8, "Klant": "Hema", "Due": 2, "Stroopwafels": 6, "Prince": 3, "Penny_wafels": 13},
-    {"Uur": 8, "Klant": "Jumbo", "Due": 2, "Stroopwafels": 0, "Prince": 8, "Penny_wafels": 19},
-    {"Uur": 8, "Klant": "AH", "Due": 3, "Stroopwafels": 0, "Prince": 5, "Penny_wafels": 30},
-    {"Uur": 8, "Klant": "Hema", "Due": 3, "Stroopwafels": 9, "Prince": 0, "Penny_wafels": 19},
-    {"Uur": 9, "Klant": "Jumbo", "Due": 4, "Stroopwafels": 0, "Prince": 3, "Penny_wafels": 13},
-    {"Uur": 9, "Klant": "AH", "Due": 4, "Stroopwafels": 0, "Prince": 4, "Penny_wafels": 13},
-    {"Uur": 10, "Klant": "Jumbo", "Due": 5, "Stroopwafels": 0, "Prince": 5, "Penny_wafels": 19},
-    {"Uur": 10, "Klant": "Hema", "Due": 5, "Stroopwafels": 6, "Prince": 0, "Penny_wafels": 25},
-    {"Uur": 11, "Klant": "Jumbo", "Due": 6, "Stroopwafels": 0, "Prince": 3, "Penny_wafels": 19},
-    {"Uur": 11, "Klant": "AH", "Due": 6, "Stroopwafels": 0, "Prince": 4, "Penny_wafels": 19},
-    {"Uur": 13, "Klant": "Hema", "Due": 7, "Stroopwafels": 4, "Prince": 0, "Penny_wafels": 25},
-    {"Uur": 13, "Klant": "AH", "Due": 7, "Stroopwafels": 0, "Prince": 5, "Penny_wafels": 19},
-    {"Uur": 14, "Klant": "Jumbo", "Due": 8, "Stroopwafels": 0, "Prince": 8, "Penny_wafels": 16},
-    {"Uur": 14, "Klant": "Hema", "Due": 8, "Stroopwafels": 4, "Prince": 0, "Penny_wafels": 20}
-]
+orders = week_1
 
 # dit moet ik nog even opslimmen..........
 
